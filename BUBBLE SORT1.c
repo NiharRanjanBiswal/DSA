d2be2f76b2de39a1c4aa8130b42023283840097d
Nihar Ranjan Biswal <95560017+NiharRanjanBiswal@users.noreply.github.com>
2022-10-31 21:12:00 +0530

    Create BUBBLE SORT1.c

---
+++ BUBBLE SORT1.c
@@ -0,0 +1,35 @@
+//ALGORITHM FOR BUBBLE SORT//
+
+#include<stdio.h>
+void bubblesort(int a[],int n)
+{
+    int i,j,temp;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+void display(int a[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int data[]={-2,34,22,65,33,12,11,3};
+    int size=(sizeof data)/sizeof(data[0]);
+    bubblesort(data,size);
+    printf("Sorted array in Ascending Order:\n");
+    display(data,size);
+}
